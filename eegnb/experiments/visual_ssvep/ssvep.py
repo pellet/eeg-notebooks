7b29e77781a6096309144df9ee8c742a7d9fd15c
Benjamin Pettit <BenjaminPettit@digimulti.com>
2023-11-27 20:46:32 +1000

    enable fixation in gaps of stimulus

--- eegnb/experiments/visual_ssvep/ssvep.py
+++ eegnb/experiments/visual_ssvep/ssvep.py
@@ -1,4 +1,3 @@
-
 from eegnb.experiments import Experiment
 import os
 from time import time
@@ -9,7 +8,6 @@ import numpy as np
 from pandas import DataFrame
 from psychopy import visual, core, event
 
-
 from eegnb.devices.eeg import EEG
 from eegnb import generate_save_fn
 from typing import Optional
@@ -17,27 +15,25 @@ from typing import Optional
 
 class VisualSSVEP(Experiment.BaseExperiment):
 
-    def __init__(self, duration=120, eeg: Optional[EEG]=None, save_fn=None, n_trials = 2010, iti = 0.5, soa = 3.0, jitter = 0.2, use_vr=False):
-        
+    def __init__(self, duration=120, eeg: Optional[EEG] = None, save_fn=None, n_trials=2010, iti=0.5, soa=3.0,
+                 jitter=0.2, use_vr=False):
+
         self.use_vr = use_vr
         exp_name = "Visual SSVEP"
-        super().__init__(exp_name, duration, eeg, save_fn, n_trials, iti, soa, jitter, use_vr)
+        super().__init__(exp_name, duration, eeg, save_fn, n_trials, iti, soa, jitter, use_vr, use_fixation=True)
 
     def load_stimulus(self):
-        
+
         grating_sf = 400 if self.use_vr else 0.2
         self.grating = visual.GratingStim(win=self.window, mask="circle", size=80, sf=grating_sf)
         self.grating_neg = visual.GratingStim(win=self.window, mask="circle", size=80, sf=grating_sf, phase=0.5)
 
-        self.fixation = visual.GratingStim(win=self.window, pos=[0, 0], sf=grating_sf, color=[1, 0, 0])
-        self.fixation.size = 0.02 if self.use_vr else 0.2
-
         # Generate the possible ssvep frequencies based on monitor refresh rate
         def get_possible_ssvep_freqs(frame_rate, stim_type="single"):
-            
+
             max_period_nb = int(frame_rate / 6)
             periods = np.arange(max_period_nb) + 1
-            
+
             if stim_type == "single":
                 freqs = dict()
                 for p1 in periods:
@@ -54,11 +50,11 @@ class VisualSSVEP(Experiment.BaseExperiment):
             return freqs
 
         def init_flicker_stim(frame_rate, cycle, soa):
-            
+
             if isinstance(cycle, tuple):
                 stim_freq = frame_rate / sum(cycle)
                 n_cycles = int(soa * stim_freq)
-            
+
             else:
                 stim_freq = frame_rate / cycle
                 cycle = (cycle, cycle)
@@ -73,10 +69,10 @@ class VisualSSVEP(Experiment.BaseExperiment):
         frame_rate = np.round(self.window.displayRefreshRate if self.use_vr else self.window.getActualFrameRate())
         freqs = get_possible_ssvep_freqs(frame_rate, stim_type="reversal")
         self.stim_patterns = [
-        init_flicker_stim(frame_rate, 2, self.soa),
-        init_flicker_stim(frame_rate, 3, self.soa),
+            init_flicker_stim(frame_rate, 2, self.soa),
+            init_flicker_stim(frame_rate, 3, self.soa),
         ]
-        
+
         print(
             (
                 "Flickering frequencies (Hz): {}\n".format(
@@ -90,8 +86,8 @@ class VisualSSVEP(Experiment.BaseExperiment):
             init_flicker_stim(frame_rate, 3, self.soa),
         ]
 
-    def present_stimulus(self, idx, trial):
-        
+    def present_stimulus(self, idx):
+
         # Select stimulus frequency
         ind = self.trials["parameter"].iloc[idx]
 
@@ -124,4 +120,8 @@ class VisualSSVEP(Experiment.BaseExperiment):
                 self.grating_neg.draw()
                 self.fixation.draw()
                 self.window.flip()
-        pass
+
+        if self.use_vr:
+            tracking_state = self.window.getTrackingState()
+            self.window.calcEyePoses(tracking_state.headPose.thePose)
+            self.window.setDefaultView()
