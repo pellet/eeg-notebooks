1a23e09f1c27e21f7af844e42bbe1f3b1d88b1d5
Benjamin Pettit <BenjaminPettit@digimulti.com>
2024-09-29 18:23:15 +1000

    boilerplate for fullfield vr pattern reversal

--- eegnb/experiments/visual_vep/pattern_reversal_vep.py
+++ eegnb/experiments/visual_vep/pattern_reversal_vep.py
@@ -10,22 +10,34 @@ from stimupy.stimuli.checkerboards import contrast_contrast
 class VisualPatternReversalVEP(Experiment.BaseExperiment):
 
     def __init__(self, duration=120, eeg: Optional[EEG] = None, save_fn=None,
-                 n_trials=2000, iti=0, soa=0.5, jitter=0, use_vr=False, size=None):
+                 n_trials=2000, iti=0, soa=0.5, jitter=0, use_vr=False, size=None, window=None):
 
         exp_name = "Visual Pattern Reversal VEP"
-        super().__init__(exp_name, duration, eeg, save_fn, n_trials, iti, soa, jitter, use_vr)
+        super().__init__(exp_name, duration, eeg, save_fn, n_trials, iti, soa, jitter, use_vr, window)
 
         self.marker_names = [1, 2]
 
         if size is None:
-            self.size = [2, 2] if self.use_vr else self.window.size
+            self.size = [2, 2] if self.use_vr else [1600, 900]
         else:
             self.size = size
 
     @staticmethod
-    def create_checkerboard(intensity_checks):
+    def create_monitor_checkerboard(intensity_checks):
         return contrast_contrast(
-            visual_size=(10, 10),  # size in degrees
+            visual_size=(15, 15),  # size in degrees
+            ppd=30,  # pixels per degree
+            frequency=(1, 1),  # spatial frequency of the checkerboard
+            intensity_checks=intensity_checks,
+            target_shape=(1, 1),
+            alpha=0,
+            tau=0,
+        )
+
+    @staticmethod
+    def create_vr_checkerboard(intensity_checks):
+        return contrast_contrast(
+            visual_size=(15, 15),  # size in degrees
             ppd=30,  # pixels per degree
             frequency=(1, 1),  # spatial frequency of the checkerboard
             intensity_checks=intensity_checks,
@@ -35,13 +47,21 @@ class VisualPatternReversalVEP(Experiment.BaseExperiment):
         )
 
     def load_stimulus(self):
-        # Create checkerboard and checkerboard inverse stimuli
-        stim1 = visual.ImageStim(self.window, image=self.create_checkerboard((1, -1))['img'], units='pix',
-                                 size=self.size, color='white')
-        stim2 = visual.ImageStim(self.window, image=self.create_checkerboard((-1, 1))['img'], units='pix',
-                                 size=self.size, color='white')
 
-        return [stim1, stim2]
+        if self.use_vr:
+            # Create VR checkerboard
+            create_checkerboard = self.create_vr_checkerboard
+
+        else:
+            # Create Monitor checkerboard
+            create_checkerboard = self.create_monitor_checkerboard
+
+        def create_checkerboard_stim(intensity_checks):
+            return visual.ImageStim(self.window,
+                                    image=create_checkerboard(intensity_checks)['img'],
+                                    units='pix', size=self.size, color='white')
+
+        return [create_checkerboard_stim((1, -1)), create_checkerboard_stim((-1, 1))]
 
     def present_stimulus(self, idx: int, trial: Any, stimuli: List[Any]):
         # onset
